0ddb27ed60c4f085e2670f36369ee52bcb69d6e8
raffleberry <raffleberry42@gmail.com>
2021-08-21 02:57:09 +0530

    code cleanup

--- src/api/v0/middlewares/index.js
+++ src/api/v0/middlewares/index.js
@@ -1,29 +1,34 @@
-module.exports = {
-  isAuthenticated: function (req, res, next) {
-    const user = req.body.userId;
-    if (user !== undefined) {
-      next();
-    } else {
-      res.status(401);
-      res.json({
-        "error": "unauthorized request"
-      });
-    }
-  },
-  isDbReady: function(req, res, next) {
-    const { db, status } = require('../../../db');
-    if (db.readyState === status.connected) {
-      next()
-    } else if (db.readyState === status.connecting) {
-      res.status(503);
-      res.json({
-        error: "Initializing connection to the database"
-      });
-    } else if (db.readyState === status.disconnected || connectionStatus === status.disconnecting) {
-      res.status(500);
-      res.json({
-        error: "Internal server error: database not connected"
-      });
-    }
+const { db, status } = require('../../../db');
+
+function isAuthenticated(req, res, next) {
+  const user = req.body.userId;
+  if (user !== undefined) {
+    next();
+  } else {
+    res.status(401);
+    res.json({
+      "error": "unauthorized request"
+    });
   }
+}
+
+function isDbReady(req, res, next) {
+  if (db.readyState === status.connected) {
+    next()
+  } else if (db.readyState === status.connecting) {
+    res.status(503);
+    res.json({
+      error: "Initializing connection to the database"
+    });
+  } else if (db.readyState === status.disconnected || connectionStatus === status.disconnecting) {
+    res.status(500);
+    res.json({
+      error: "Internal server error: database not connected"
+    });
+  }
+}
+
+module.exports = {
+  isAuthenticated,
+  isDbReady
 }
